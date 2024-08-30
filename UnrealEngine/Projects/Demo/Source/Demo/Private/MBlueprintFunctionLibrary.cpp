41d2075b58c60a631f83888c2a1d5abcc76f051c
RubbingHandsFly <36793378+realAYAYA@users.noreply.github.com>
2024-08-30 19:43:20 +0800

    RGBA8 to Float4

--- UnrealEngine/Projects/Demo/Source/Demo/Private/MBlueprintFunctionLibrary.cpp
+++ UnrealEngine/Projects/Demo/Source/Demo/Private/MBlueprintFunctionLibrary.cpp
@@ -1,2 +1,19 @@
-#include "D:\code-check\UnrealEngine-ToonLit\UnrealEngine\Engine\Intermediate\Build\Win64\x64\UnrealEditorGPF\Development\UnrealEd\SharedPCH.UnrealEd.Project.NoValFmtStr.ValApi.Cpp20.InclOrderUnreal5_3.h"
 #include "MBlueprintFunctionLibrary.h"
+
+int32 UMBlueprintFunctionLibrary::ConvertFloat4ToRGBA8(FVector4 V)
+{
+	return (uint32(V.W) & 0x000000FF) << 24U |
+		(uint32(V.Z) & 0x000000FF) << 16U |
+			(uint32(V.Y) & 0x000000FF) << 8U |
+				uint32(V.X) & 0x000000FF;
+}
+
+FVector4 UMBlueprintFunctionLibrary::ConvertRGBA8ToFloat4(int32 V)
+{
+	uint32 Val = V;
+	return FVector4(
+		float(Val & 0x000000FF),
+		float((Val & 0x0000FF00) >> 8U),
+		float((Val & 0x00FF0000) >> 16U),
+		float((Val & 0xFF000000) >> 24U));
+}
--- UnrealEngine/Projects/Demo/Source/Demo/Public/MBlueprintFunctionLibrary.h
+++ UnrealEngine/Projects/Demo/Source/Demo/Public/MBlueprintFunctionLibrary.h
@@ -1,7 +1,20 @@
 #pragma once
+#include "MBlueprintFunctionLibrary.generated.h"
 
-class MBlueprintFunctionLibrary
+UCLASS()
+class UMBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
 {
+
+	GENERATED_BODY()
+
+	
 public:
+
+
+	UFUNCTION(BlueprintCallable, Category = "TestM")
+	static int32 ConvertFloat4ToRGBA8(FVector4 V);
+
+	UFUNCTION(BlueprintCallable, Category = "TestM")
+	static FVector4 ConvertRGBA8ToFloat4(int32 V);
 	
 };
